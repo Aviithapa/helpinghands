70740534c53a7d0ac03ae3c3ae496055f3e3f889
Rijan984 <rijan18shrestha@gmail.com>
2020-06-24 10:16:21 +0545

    Design and backend setup of pages

---
+++ resources/views/web/pages/single-blog.blade.php
@@ -0,0 +1,137 @@
+@extends('web.layouts.app')
+
+
+
+@section('content')
+    <link rel="stylesheet" href="{{asset('frontassets/css/eventstyle.css')}}">
+    <!--Bandicamp-->
+    @foreach($blog as $blog)
+        <section class="hero-wrap hero-wrap-2" style="background-image: url('{{$blog->getNewsImage()}}');" data-stellar-background-ratio="0.5">
+            <div class="overlay"></div>
+            <div class="container">
+                <div class="row no-gutters slider-text align-items-center justify-content-center">
+                    <div class="col-md-9 ftco-animate text-center">
+                        <h1 class="mb-2 bread">Blog</h1>
+                        <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About us <i class="ion-ios-arrow-forward"></i></span></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!--End Bandicamp-->
+
+
+        <section class="blog_area single-post-area section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 posts-list">
+                        <div class="single-post">
+                            <div class="feature-img">
+                                <img class="img-fluid" src="{{$blog->getNewsImage()}}" alt="">
+                            </div>
+                            <div class="blog_details">
+                                <h2>{{$blog->title}}
+                                </h2>
+                                <ul class="blog-info-link mt-3 mb-4">
+                                    <li><a href="#"><i class="fa fa-user"></i> Travel, Lifestyle</a></li>
+                                    <li><a href="#"><i class="fa fa-comments"></i> 03 Comments</a></li>
+                                </ul>
+                                <p class="excert">
+                                    {{$blog->content}}
+                                </p>
+                                <div class="quote-wrapper">
+                                    <div class="quotes">
+                                        {{$blog->excerpt}}
+                                    </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                        </div>
+                        <div class="navigation-top">
+                            <div class="d-sm-flex justify-content-between text-center">
+                                <p class="like-info"><span class="align-middle"><i class="fa fa-heart"></i></span> Lily and 4
+                                    people like this</p>
+                                <div class="col-sm-4 text-center my-2 my-sm-0">
+                                    <!-- <p class="comment-count"><span class="align-middle"><i class="fa fa-comment"></i></span> 06 Comments</p> -->
+                                </div>
+                                <ul class="social-icons">
+                                    <li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="blog-author">
+                            <div class="media align-items-center">
+                                <img src="img/blog/author.png" alt="">
+                                <div class="media-body">
+                                    <a href="#">
+                                        <h4>Harvard milan</h4>
+                                    </a>
+                                    <p>Second divided from form fish beast made. Every of seas all gathered use saying you're, he
+                                        our dominion twon Second divided from</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="blog_right_sidebar">
+                            <aside class="single_sidebar_widget popular_post_widget">
+                                <h3 class="widget_title">Recent Post</h3>
+                                @foreach($blogs as $blog)
+                                    <div class="media post_item">
+                                        <img src="{{$blog->getNewsImage()}}" alt="{{getSiteSetting('site_title') != null? getSiteSetting('site_title'): ''}}" style="height:50px;width: 50px;">
+                                        <div class="media-body">
+                                            <a href="{{url('single-blog/'.$blog->slug)}}">
+                                                <h3>{{str_limit($blog['title'],100)}}</h3>
+                                            </a>
+                                            <p>{{$blog->created_at}}</p>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </aside>
+                            <aside class="single_sidebar_widget instagram_feeds">
+                                <h4 class="widget_title">Instagram Feeds</h4>
+                                <ul class="instagram_row flex-wrap">
+                                    <li>
+                                        <a href="#">
+                                            <img class="img-fluid" src="img/post/post_5.png" alt="">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <img class="img-fluid" src="img/post/post_6.png" alt="">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <img class="img-fluid" src="img/post/post_7.png" alt="">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <img class="img-fluid" src="img/post/post_8.png" alt="">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <img class="img-fluid" src="img/post/post_9.png" alt="">
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <img class="img-fluid" src="img/post/post_10.png" alt="">
+                                        </a>
+                                    </li>
+                                </ul>
+                            </aside>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================ Blog Area end =================-->
+
+@endsection
